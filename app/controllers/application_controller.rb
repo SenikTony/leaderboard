c55bbda8237777c61ee46d7353d035e7492812e0
Anton Senik <ansenik@yandex.ru>
2023-02-26 19:27:26 +0400

    Added activeadmin.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  def authenticate_admin_user!
+    authenticate_or_request_with_http_basic("Admin Dashboard") do |name, password|
+      name == ENV["ADMIN_USER"] && password == ENV["ADMIN_PASSWORD"]
+    end
+  end
 end
